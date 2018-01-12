fad5dda7042369cabcc3922ab55407124c6c8028
gercks <elizagercke@gmail.com>
2018-01-12 13:00:46 -0500

    Initial commit
    Change remote origin
    
    Update project name in several files

--- config/environment.js
+++ config/environment.js
@@ -2,11 +2,12 @@
 
 module.exports = function (environment) {
   'use strict';
-  const ENV = {
-    modulePrefix: 'ga-wdi-boston.ember-auth',
+  var ENV = {
+    modulePrefix: 'queeresources',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
+    apiHost: 'http://localhost:3000/',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -49,7 +50,9 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
-    ENV.locationType = 'hash';
+  ENV.rootURL = '/QueeResources-Front-End';
+  ENV.locationType = 'hash';
+  ENV.apiHost = 'https://fast-spire-12567.herokuapp.com/';
   }
 
   return ENV;
