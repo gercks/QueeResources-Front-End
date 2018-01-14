9576268965c76a8d21caa6654d1b8f9f0366c51b
gercks <elizagercke@gmail.com>
2018-01-14 17:11:28 -0500

    Add link to resources list that displays dummy data
    -link to queeresources page from my-application
    -display a list of dummy data with links to each data point

--- app/components/queer-resource.js
+++ app/components/queer-resource.js
@@ -1,4 +1,38 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  model: function () {
+return [
+  {
+    id: 1,
+    name: 'Bob',
+    hobby: 'having little lambs'
+  },
+  {
+    id: 2,
+    name: 'Eliza',
+    hobby: 'hugging trees'
+  },
+  {
+    id: 3,
+    name: 'KJ',
+    hobby: 'modelling'
+  },
+  {
+    id: 4,
+    name: 'Sally',
+    hobby: 'mustangs'
+  },
+  {
+    id: 5,
+    name: 'Barb',
+    hobby: 'idk Barb'
+  },
+  {
+    id: 6,
+    name: 'Oprah',
+    hobby: 'running for president 2020'
+  },
+];
+}
 });
