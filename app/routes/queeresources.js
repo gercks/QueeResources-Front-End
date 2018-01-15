984fdaea5894a1462d223db691794d85d99df451
gercks <elizagercke@gmail.com>
2018-01-15 12:38:49 -0500

    Fix naming issues; establish working route to queeresources
    -change from queer-resource to queeresouce in routes, components, models,
    tests, and templates
    
    -create queeresouce-form for creating new resources (but doesn't function
    yet)
    -begin building crud for index and create

--- app/routes/queeresources.js
+++ app/routes/queeresources.js
@@ -1,38 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function () {
-return [
-  {
-    id: 1,
-    name: 'Bob',
-    hobby: 'having little lambs'
-  },
-  {
-    id: 2,
-    name: 'Eliza',
-    hobby: 'hugging trees'
-  },
-  {
-    id: 3,
-    name: 'KJ',
-    hobby: 'modelling'
-  },
-  {
-    id: 4,
-    name: 'Sally',
-    hobby: 'mustangs'
-  },
-  {
-    id: 5,
-    name: 'Barb',
-    hobby: 'idk Barb'
-  },
-  {
-    id: 6,
-    name: 'Oprah',
-    hobby: 'running for president 2020'
-  },
-];
-}
+  model () {
+  // findAll makes a GET request to /queeresources
+  return this.get('store').findAll('queeresource');
+},
+// actions: {
+  // createQr(queer-resource) {
+  //   console.log(queer-resource);
+  //   let newList = this.get('store').createRecord('list', list);
+  //   console.log(newList.get('isEmpty'))
+    // newList.save();
+  // },
+  // deleteList(list) {
+  //   list.deleteRecord();
+  //   list.save();
+  // },
+// }
+
 });
