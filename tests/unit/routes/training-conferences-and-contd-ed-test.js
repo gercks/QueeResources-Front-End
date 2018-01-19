ad46e00e689509639d4fd5e307bae7e30723274c
gercks <elizagercke@gmail.com>
2018-01-18 22:41:53 -0500

    Generate routes to search by orgtype, styles
    -add minimal styling
    -create nine routes and buttons so users can search by type of resource

---
+++ tests/unit/routes/training-conferences-and-contd-ed-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:training-conferences-and-contd-ed', 'Unit | Route | training conferences and contd ed', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
