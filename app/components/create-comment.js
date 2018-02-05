3a9e78cec679cca50696ae6636c1dc510e8d0a18
gercks <elizagercke@gmail.com>
2018-02-05 12:06:25 -0500

    Add create comment action, links to queeresource
    -check functionality once deployed

--- app/components/create-comment.js
+++ app/components/create-comment.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  auth: Ember.inject.service(),
+  flashMessages: Ember.inject.service(),
+ newComment: {
+   text: null,
+ },
+ // message: null,
+ actions: {
+   createComment () {
+     if ((this.get('newComment.text') !== null)) {
+       this.sendAction('createComment', this.get('newComment'), this.get('queeresource'));
+       this.set('newComment.text', null);
+       // this.set('message', null);
+     } else {
+       this.get('flashMessages')
+       .danger('Please write a comment before submitting');
+     }
+   }
+ }
 });
--- app/routes/queeresource.js
+++ app/routes/queeresource.js
@@ -1,11 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  flashMessages: Ember.inject.service(),
+  user: Ember.computed.alias('auth.credentials.id'),
   model (params) {
     // findRecord makes a GET request to /lists/:list_id
     // the api returns a single list
     // ember uses the models/list.js to build an ember list object with that data
     // and returns it as model to the template
     return this.get('store').findRecord('queeresource', params.queeresource_id);
+  },
+  actions: {
+    createComment(comment, queeresource) {
+      let newComment = this.get('store').createRecord('comment', comment);
+      queeresource.get('comments').pushObject(newComment);
+      newComment.save();
+      this.get('flashMessages').success('comment posted');
+    },
   },
 });
