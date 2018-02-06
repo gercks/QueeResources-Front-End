aa5fbef25806f684025681243327e0ee5c975f3a
gercks <elizagercke@gmail.com>
2018-02-06 10:18:32 -0500

    Add styling and comment editable round 1
    -editable may not work, deploying to check
    -orgtypes are now styled better
    -switch button to my queeresources to say sign up/in if not logged in

--- app/routes/queeresources.js
+++ app/routes/queeresources.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   flashMessages: Ember.inject.service(),
+  auth: Ember.inject.service(),
   model () {
   // findAll makes a GET request to /queeresources
   return this.get('store').findAll('queeresource');
