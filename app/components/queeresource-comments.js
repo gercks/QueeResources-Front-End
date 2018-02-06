62117fcc33c414aea9e0e517d7ccce85ae3f94bb
gercks <elizagercke@gmail.com>
2018-02-06 12:08:41 -0500

    Fix comment editable

--- app/components/queeresource-comments.js
+++ app/components/queeresource-comments.js
@@ -2,9 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     auth: Ember.inject.service(),
-  model () {
-  return this.get('store').findAll('comment');
-},
+//   model () {
+//   return this.get('store').findAll('comment');
+// },
 actions: {
   deleteComment (comment) {
         comment.deleteRecord();
