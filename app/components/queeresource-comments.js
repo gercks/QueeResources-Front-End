072b4a4b4c3bd7d6d12b1c837b0d9d3044c4acfe
gercks <elizagercke@gmail.com>
2018-02-12 17:26:11 -0500

    Update comments functionality

--- app/components/queeresource-comments.js
+++ app/components/queeresource-comments.js
@@ -2,13 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     auth: Ember.inject.service(),
-  model () {
-  return this.get('store').findAll('comment');
-},
+    classNames: ['updateform'],
+    classNameBindings: ['updateformhidden'],
+    updateformhidden: true,
+//   model () {
+//   return this.get('store').findAll('comment');
+// },
 actions: {
   deleteComment (comment) {
         comment.deleteRecord();
         comment.save();
       },
-}
+    }
 });
